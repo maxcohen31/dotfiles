13be3259c5416010329e261b8859e2552d5e04c4
maxcohen31 <eman_93@hotmail.it>
2023-06-02 10:07:24 +0200

    adding dotfiles

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,35 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.o.number = true
+vim.o.signcolumn = 'yes'
+
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+local opts = { noremap = true, silent = true }
+-- Normal-mode commands
+vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
+vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
+
+-- Visual-mode commands
+vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
---
+++ nvim/lua/core/plugin_config/lualine.lua
@@ -0,0 +1,22 @@
+-- gruvbox
+--require('lualine').setup {
+ --options = {
+ --   icons_enabled = true,
+-- theme = 'gruvbox',
+--  },
+  --sections = {
+    --lualine_a = {
+      --{
+        --'filename',
+       -- path = 1,
+      --}
+    --}
+  --}   
+-- }
+
+--require('lualine').setup {
+--    options = {
+--        theme = "catppuccin-mocha"
+        -- ... the rest of your lualine config
+  --  }
+--}
