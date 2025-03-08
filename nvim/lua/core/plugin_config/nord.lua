9d019cacfbb17849f7fd4784a0ec227528c8a77e
maxcohen31 <eman_93@hotmail.it>
2025-03-08 18:36:47 +0100

    nord

---
+++ nvim/lua/core/plugin_config/nord.lua
@@ -0,0 +1,10 @@
+require('lualine').setup {
+  options = {
+    -- ... your lualine config
+    theme = 'nord'
+    -- ... your lualine config
+  }
+}
+
+
+vim.cmd[[colorscheme nord]]
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -15,9 +15,10 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   -- use 'ellisonleao/gruvbox.nvim'
   use 'folke/tokyonight.nvim'
-  use "EdenEast/nightfox.nvim"
+  --use "EdenEast/nightfox.nvim"
   --use "rebelot/kanagawa.nvim"
   -- use { "catppuccin/nvim", as = "catppuccin" }
+  use 'shaunsingh/nord.nvim'
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {'nvim-tree/nvim-web-devicons'}
