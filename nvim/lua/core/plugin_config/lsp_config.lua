0d8a91109c48531174f5a3f10f49643b849c2210
maxcohen31 <eman_93@hotmail.it>
2025-10-05 10:40:38 +0200

    lsp_config.lua updated

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -68,3 +68,101 @@ lspconfig.ts_ls.setup {
      capabilities = capabilities,
      on_attach = on_attach,
  })
+local lsp_utils = {}
+
+-- on_attach function
+local on_attach = function(_, _)
+  local map = vim.keymap.set
+  local opts = { noremap = true, silent = true }
+
+  map('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  map('n', 'gd', vim.lsp.buf.definition, opts)
+  map('n', 'gi', vim.lsp.buf.implementation, opts)
+  map('n', 'gr', require('telescope.builtin').lsp_references, opts)
+  map('n', 'K', vim.lsp.buf.hover, opts)
+end
+
+-- capabilities
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- Setup mason and mason-lspconfig
+require('mason').setup()
+
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    "lua_ls",
+    "pyright",
+    "clangd",
+    "ts_ls",
+    "jdtls",
+  },
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+
+    ["lua_ls"] = function()
+      require("lspconfig").lua_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            }
+          }
+        }
+      })
+    end,
+
+    ["pyright"] = function()
+      require("lspconfig").pyright.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "workspace",
+              useLibraryCodeForTypes = true
+            }
+          }
+        }
+      })
+    end,
+
+    ["ts_ls"] = function()
+      require("lspconfig").ts_ls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        init_options = {
+          preferences = {
+            disableSuggestions = true
+          }
+        }
+      })
+    end,
+
+    ["jdtls"] = function()
+      require("lspconfig").jdtls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+
+    ["clangd"] = function()
+      require("lspconfig").clangd.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        flags = {
+          debounce_text_changes = 150,
+        }
+      })
+    end,
+  }
+})
+
