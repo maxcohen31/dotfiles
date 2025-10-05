1a44a48bc0535721f183a8298c61948a8b9ced08
maxcohen31 <eman_93@hotmail.it>
2025-10-05 10:41:38 +0200

    plugins.lua updated

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -72,3 +72,76 @@ return require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- use 'ellisonleao/gruvbox.nvim'
+  -- use 'folke/tokyonight.nvim'
+  --use "EdenEast/nightfox.nvim"
+  --use "rebelot/kanagawa.nvim"
+  use 'shaunsingh/nord.nvim'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {'nvim-tree/nvim-web-devicons'}
+      }
+  use 'nvim-lualine/lualine.nvim'
+  use 'vim-test/vim-test'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'windwp/nvim-autopairs'
+  use "akinsho/toggleterm.nvim"
+  -- completion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use "rafamadriz/friendly-snippets"
+  use{
+    "williamboman/mason.nvim",
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig'
+     }
+  use {
+	  'nvim-telescope/telescope.nvim',
+	  tag = '0.1.8',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+      }
+  use {
+      'goolord/alpha-nvim',
+      requires = {
+          --'echasnovski/mini.icons',
+         'nvim-lua/plenary.nvim'
+      },
+    config = function ()
+        --require'alpha'.setup(require'alpha.themes.startify'.config)
+        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    end
+    }
+
+    use { "nvim-neotest/nvim-nio" }
+    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} }
+    -- java development
+    use 'mfussenegger/nvim-jdtls'
+    use 'mfussenegger/nvim-dap'
+
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
