d015e234e3e6dffce21c48354c4f4640e8295932
maxcohen31 <eman_93@hotmail.it>
2025-12-05 17:34:51 +0100

    added Ocaml to treesitter.lua

--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = {"cpp", "c", "lua", "rust", "ruby", "vim", "python" },
+  ensure_installed = {"cpp", "c", "lua", "rust", "ruby", "vim", "python", "ocaml", "ocaml_interface" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
